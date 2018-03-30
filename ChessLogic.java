a5eba49732182888e7af749509c108da49cce31f
Kashnox <ebainbridge98@gmail.com>
2018-03-30 09:16:55 -0400

    Update ChessLogic.java

--- ChessLogic.java
+++ ChessLogic.java
@@ -6,12 +6,11 @@ public class ChessLogic {
 	//ChessLogic.Piece piece; import static whatever.something.ChessLogic.*;
 	//This is a Christian logic class, so please, NO swearing!
 	public static abstract class Piece {
-		public static 
 		boolean white;
 		public Piece(boolean isWhite) {
 			white=isWhite;
 		}
-		abstract HashSet<Integer> getMoves(int loc, Piece[][] board);
+		abstract HashSet<Integer> getMoves(int loc, Piece[][] board, boolean forReal);
 		boolean inBounds(int r,int c) {
 			return r>=0&&r<8&&c>=0&&c<8;
 		}
@@ -22,7 +21,7 @@ public class ChessLogic {
 		public Pawn(boolean isWhite) {
 			super(isWhite);
 		}
-		HashSet<Integer> getMoves(int loc, Piece[][] board){
+		HashSet<Integer> getMoves(int loc, Piece[][] board,boolean forReal){
 			HashSet<Integer> moves=new HashSet<Integer>();
 			int r=loc/8,c=loc%8;
 			
@@ -33,44 +32,26 @@ public class ChessLogic {
 				start=1;
 			}
 			if(inBounds(r+dr,c)&&board[r+dr][c]==null) {
-				board[r+dr][c]=board[r][c];
-				board[r][c]=null;
-				if(!inCheck(board[r+dr][c].white,board))
+				if(!forReal||checkValidity(r,c,r+dr,c,board))
 					moves.add((r+dr)*8+c);
-				board[r][c]=board[r+dr][c];
-				board[r+dr][c]=null;
 
 				//Pawns can move two squares on their first turn.
 				if(r==start) {
 					if(inBounds(r+2*dr,c)&&board[r+2*dr][c]==null) {
-						board[r+2*dr][c]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r+2*dr][c].white,board))
+						if(!forReal||checkValidity(r,c,r+2*dr,c,board))
 							moves.add((r+2*dr)*8+c);
-						board[r][c]=board[r+2*dr][c];
-						board[r+2*dr][c]=null;
 					}
 				}
 			}
 
 			//Check if it can capture
 			if(inBounds(r+dr,c-1)&&board[r+dr][c-1]!=null&&board[r+dr][c-1].white!=white) {
-				Piece temp=board[r+dr][c-1];
-				board[r+dr][c-1]=board[r][c];
-				board[r][c]=null;
-				if(!inCheck(board[r+dr][c-1].white,board))
+				if(!forReal||checkValidity(r,c,r+dr,c-1,board))
 					moves.add((r+dr)*8+c-1);
-				board[r][c]=board[r+dr][c-1];
-				board[r+dr][c-1]=temp;
 			}
 			if(inBounds(r+dr,c+1)&&board[r+dr][c+1]!=null&&board[r+dr][c+1].white!=white) {
-				Piece temp=board[r+dr][c+1];
-				board[r+dr][c+1]=board[r][c];
-				board[r][c]=null;
-				if(!inCheck(board[r+dr][c+1].white,board))
+				if(!forReal||checkValidity(r+dr,c,r,c+1,board))
 					moves.add((r+dr)*8+c+1);
-				board[r][c]=board[r+dr][c+1];
-				board[r+dr][c+1]=temp;
 			}
 			
 			return moves;
@@ -82,7 +63,7 @@ public class ChessLogic {
 		public King(boolean isWhite) {
 			super(isWhite);
 		}
-		HashSet<Integer> getMoves(int loc,Piece[][] board) {
+		HashSet<Integer> getMoves(int loc,Piece[][] board,boolean forReal) {
 			int r=loc/8,c=loc%8;
 			int[] dr= {-1,-1,0,1,1,1,0,-1},dc= {0,1,1,1,0,-1,-1,-1};
 			HashSet<Integer> moves=new HashSet<Integer>();
@@ -91,13 +72,8 @@ public class ChessLogic {
 			for(int i=0;i<8;i++) {
 				int r2=r+dr[i],c2=c+dc[i];
 				if(inBounds(r2,c2)&&(board[r2][c2]==null||board[r2][c2].white!=white)) {
-					Piece temp=board[r2][c2];
-					board[r2][c2]=board[r][c];
-					board[r][c]=null;
-					if(!inCheck(board[r2][c2].white,board))
+					if(!forReal||checkValidity(r,c,r2,c2,board))
 						moves.add(r2*8+c2);
-					board[r][c]=board[r2][c2];
-					board[r2][c2]=temp;
 				}
 			}
 			return moves;
@@ -109,20 +85,15 @@ public class ChessLogic {
 		public Knight(boolean isWhite) {
 			super(isWhite);
 		}
-		HashSet<Integer> getMoves(int loc,Piece[][] board){
+		HashSet<Integer> getMoves(int loc,Piece[][] board,boolean forReal){
 			int r=loc/8,c=loc%8;
 			int[] dr= {-2,-1,1,2,2,1,-1,-2}, dc= {1,2,2,1,-1,-2,-2,-1};
 			HashSet<Integer> moves=new HashSet<Integer>();
 			for(int i=0;i<8;i++) {
 				int r2=r+dr[i],c2=c+dc[i];
 				if(inBounds(r2,c2)&&(board[r2][c2]==null||board[r2][c2].white!=white)) {
-					Piece temp=board[r2][c2];
-					board[r2][c2]=board[r][c];
-					board[r][c]=null;
-					if(!inCheck(board[r2][c2].white,board))
+					if(!forReal||checkValidity(r,c,r2,c2,board))
 						moves.add(r2*8+c2);
-					board[r][c]=board[r2][c2];
-					board[r2][c2]=temp;
 				}
 			}
 			return moves;
@@ -134,7 +105,7 @@ public class ChessLogic {
 		public Rook(boolean isWhite) {
 			super(isWhite);
 		}
-		HashSet<Integer> getMoves(int loc,Piece[][] board) {
+		HashSet<Integer> getMoves(int loc,Piece[][] board,boolean forReal) {
 			int r=loc/8,c=loc%8;
 			int[] dr= {-1,0,1,0},dc= {0,1,0,-1};
 			HashSet<Integer> moves=new HashSet<Integer>();
@@ -147,23 +118,14 @@ public class ChessLogic {
 					if(!inBounds(r2,c2))
 						break;
 					if(board[r2][c2]==null) {
-						board[r2][c2]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r2][c2].white,board))
+						if(!forReal||checkValidity(r,c,r2,c2,board))
 							moves.add(r2*8+c2);
-						board[r][c]=board[r2][c2];
-						board[r2][c2]=null;
 						continue;
 					}
 					//Capture
 					if(board[r2][c2].white!=white) {
-						Piece temp=board[r2][c2];
-						board[r2][c2]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r2][c2].white,board))
+						if(!forReal||checkValidity(r,c,r2,c2,board))
 							moves.add(r2*8+c2);
-						board[r][c]=board[r2][c2];
-						board[r2][c2]=temp;
 					}
 					
 					//If there was a Piece here, the rook can't go any further in this direction.
@@ -179,7 +141,7 @@ public class ChessLogic {
 		public Bishop(boolean isWhite) {
 			super(isWhite);
 		}
-		HashSet<Integer> getMoves(int loc,Piece[][] board){
+		HashSet<Integer> getMoves(int loc,Piece[][] board,boolean forReal){
 			int r=loc/8,c=loc%8;
 			int[] dr= {-1,1,1,-1},dc= {1,1,-1,-1};
 			HashSet<Integer> moves=new HashSet<Integer>();
@@ -192,24 +154,14 @@ public class ChessLogic {
 					if(!inBounds(r2,c2))
 						break;
 					if(board[r2][c2]==null) {
-						Piece temp=board[r2][c2];
-						board[r2][c2]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r2][c2].white,board))
+						if(!forReal||checkValidity(r,c,r2,c2,board))
 							moves.add(r2*8+c2);
-						board[r][c]=board[r2][c2];
-						board[r2][c2]=temp;
 						continue;
 					}
 					//Capture
 					if(board[r2][c2].white!=white) {
-						Piece temp=board[r2][c2];
-						board[r2][c2]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r2][c2].white,board))
+						if(!forReal||checkValidity(r,c,r2,c2,board))
 							moves.add(r2*8+c2);
-						board[r][c]=board[r2][c2];
-						board[r2][c2]=temp;
 					}
 					
 					break;
@@ -226,7 +178,7 @@ public class ChessLogic {
 		}
 		
 		//This is basically a rook with a king's dr/dc array.
-		HashSet<Integer> getMoves(int loc,Piece[][] board){
+		HashSet<Integer> getMoves(int loc,Piece[][] board,boolean forReal){
 			int r=loc/8,c=loc%8;
 			int[] dr= {-1,-1,0,1,1,1,0,-1},dc= {0,1,1,1,0,-1,-1,-1};
 			HashSet<Integer> moves=new HashSet<Integer>();
@@ -238,24 +190,14 @@ public class ChessLogic {
 					if(!inBounds(r2,c2))
 						break;
 					if(board[r2][c2]==null) {
-						Piece temp=board[r2][c2];
-						board[r2][c2]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r2][c2].white,board))
+						if(!forReal||checkValidity(r,c,r2,c2,board))
 							moves.add(r2*8+c2);
-						board[r][c]=board[r2][c2];
-						board[r2][c2]=temp;
 						continue;
 					}
 					//Capture
 					if(board[r2][c2].white!=white) {
-						Piece temp=board[r2][c2];
-						board[r2][c2]=board[r][c];
-						board[r][c]=null;
-						if(!inCheck(board[r2][c2].white,board))
+						if(!forReal||checkValidity(r,c,r2,c2,board))
 							moves.add(r2*8+c2);
-						board[r][c]=board[r2][c2];
-						board[r2][c2]=temp;
 					}
 					break;
 				}
@@ -275,12 +217,24 @@ public class ChessLogic {
 		return -1;
 	}
 
+	static boolean checkValidity(int r,int c,int r2,int c2,Piece[][] board) {
+		boolean good=false;
+		Piece temp=board[r2][c2];
+		board[r2][c2]=board[r][c];
+		board[r][c]=null;
+		if(!inCheck(board[r2][c2].white,board))
+			good=true;
+		board[r][c]=board[r2][c2];
+		board[r2][c2]=temp;
+		return good;
+	}
+	
 	static boolean inCheck(boolean color,Piece[][] board) {
 		int loc=findKing(color,board);
 		for(int i=0;i<8;i++) {
 			for(int j=0;j<8;j++) {
 				if(board[i][j]!=null&&board[i][j].white!=color) {
-					HashSet<Integer> moves=board[i][j].getMoves(i*8+j, board);
+					HashSet<Integer> moves=board[i][j].getMoves(i*8+j, board,false);
 					if(moves.contains(loc))
 						return true;
 				}
@@ -295,7 +249,7 @@ public class ChessLogic {
 		for(int i=0;i<8;i++) {
 			for(int j=0;j<8;j++) {
 				if(board[i][j]!=null&&board[i][j].white==color) {
-					if(board[i][j].getMoves(i*8+j,board).size()!=0)
+					if(board[i][j].getMoves(i*8+j,board,true).size()!=0)
 						return 0;
 				}
 			}

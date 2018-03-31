0c43bea4531338dd85f72fe5642fa9a96edb0213
Timothy Yu <amasadpanda@gmail.com>
2018-03-31 14:32:28 -0400

    2nd commit

---
+++ Server/Server/src/main/java/UserCreator.java
@@ -0,0 +1,12 @@
+import com.google.firebase.database.FirebaseDatabase;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserCreator extends FireEater{
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response) {
+        FirebaseDatabase getDatabase()
+    }
+}

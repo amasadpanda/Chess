1c149d00ecdc9e4d44e32385f6cb96d2335fa1f6
warmCabin <ryanwb2@gmail.com>
2018-04-16 17:46:34 -0400

    Moved board initializers to the bottom
    I am bored and I miss Philip.

--- Android/ChessWithHats/app/src/main/java/com/group8/chesswithhats/util/ChessLogic.java
+++ Android/ChessWithHats/app/src/main/java/com/group8/chesswithhats/util/ChessLogic.java
@@ -6,102 +6,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class ChessLogic {
-
-    public static Piece[][] makeBasicChessBoard(){
-        Piece[][] board=new Piece[8][8];
-        for(int i=0;i<8;i++){
-            board[1][i]=new Pawn(false);
-            board[6][i]=new Pawn(true);
-        }
-        //Black
-        board[0][0]=new Rook(false);
-        board[0][1]=new Knight(false);
-        board[0][2]=new Bishop(false);
-        board[0][3]=new Queen(false);
-        board[0][4]=new King(false);
-        board[0][5]=new Bishop(false);
-        board[0][6]=new Knight(false);
-        board[0][7]=new Rook(false);
-        
-        //White
-        board[7][0]=new Rook(true);
-        board[7][1]=new Knight(true);
-        board[7][2]=new Bishop(true);
-        board[7][3]=new Queen(true);
-        board[7][4]=new King(true);
-        board[7][5]=new Bishop(true);
-        board[7][6]=new Knight(true);
-        board[7][7]=new Rook(true);
-        
-        return board;
-    }
-    
-    public static Piece[][] makePeasantRevoltBoard(){
-        Piece[][] board=new Piece[8][8];
-        for(int i=0;i<8;i++)
-            board[6][i]=new Pawn(true);
-        board[7][4]=new King(true);
-        
-        board[0][1]=new Knight(false);
-        board[0][2]=new Knight(false);
-        board[0][4]=new King(false);
-        board[0][6]=new Knight(false);
-        board[1][4]=new Pawn(false);
-        
-        return board;
-    }
-    
-    public static Piece[][] makeRandomizedBoardThatIsAValidChess960Board(){
-    	Piece[][] board=new Piece[8][8];
-    	for(int i=0;i<8;i++) {
-    		board[1][i]=new Pawn(false);
-    		board[6][i]=new Pawn(true);
-    	}
-    	ArrayList<Integer> spotsLeft=new ArrayList<Integer>();
-    	for(int i=0;i<8;i++)
-    		spotsLeft.add(i);
-    	
-    	//Bishop on even
-    	int index=(int)(Math.random()*4)*2;
-    	board[0][index]=new Bishop(false);
-    	board[7][index]=new Bishop(true);
-    	spotsLeft.remove((Object)new Integer(index));
-    	
-    	//Bishop on odd
-    	index=(int)(Math.random()*4)*2+1;
-    	board[0][index]=new Bishop(false);
-    	board[7][index]=new Bishop(true);
-    	spotsLeft.remove((Object)new Integer(index));
-    	
-    	//Queen
-    	index=(int)(Math.random()*6);
-    	index=spotsLeft.get(index);
-    	board[0][index]=new Queen(false);
-    	board[7][index]=new Queen(true);
-    	spotsLeft.remove((Object)new Integer(index));
-    	
-    	//Knights
-    	index=(int)(Math.random()*5);
-    	index=spotsLeft.get(index);
-    	board[0][index]=new Knight(false);
-    	board[7][index]=new Knight(true);
-    	spotsLeft.remove((Object)new Integer(index));
-    	index=(int)(Math.random()*4);
-    	index=spotsLeft.get(index);
-    	board[0][index]=new Knight(false);
-    	board[7][index]=new Knight(true);
-    	spotsLeft.remove((Object)new Integer(index));
-    	
-    	//Everything else
-    	board[0][spotsLeft.get(0)]=new Rook(false);
-    	board[0][spotsLeft.get(1)]=new King(false);
-    	board[0][spotsLeft.get(2)]=new Rook(false);
-    	board[7][spotsLeft.get(0)]=new Rook(true);
-    	board[7][spotsLeft.get(1)]=new King(true);
-    	board[7][spotsLeft.get(2)]=new Rook(true);
-        
-        return board;
-    }
     
     //Base Piece Class
     //ChessLogic.Piece piece; import static whatever.something.ChessLogic.*;
@@ -645,4 +549,101 @@ public class ChessLogic {
             return 1;
         return 2;
     }
+
+    public static Piece[][] makeBasicChessBoard(){
+        Piece[][] board=new Piece[8][8];
+        for(int i=0;i<8;i++){
+            board[1][i]=new Pawn(false);
+            board[6][i]=new Pawn(true);
+        }
+        //Black
+        board[0][0]=new Rook(false);
+        board[0][1]=new Knight(false);
+        board[0][2]=new Bishop(false);
+        board[0][3]=new Queen(false);
+        board[0][4]=new King(false);
+        board[0][5]=new Bishop(false);
+        board[0][6]=new Knight(false);
+        board[0][7]=new Rook(false);
+
+        //White
+        board[7][0]=new Rook(true);
+        board[7][1]=new Knight(true);
+        board[7][2]=new Bishop(true);
+        board[7][3]=new Queen(true);
+        board[7][4]=new King(true);
+        board[7][5]=new Bishop(true);
+        board[7][6]=new Knight(true);
+        board[7][7]=new Rook(true);
+
+        return board;
+    }
+
+    public static Piece[][] makePeasantRevoltBoard(){
+        Piece[][] board=new Piece[8][8];
+        for(int i=0;i<8;i++)
+            board[6][i]=new Pawn(true);
+        board[7][4]=new King(true);
+
+        board[0][1]=new Knight(false);
+        board[0][2]=new Knight(false);
+        board[0][4]=new King(false);
+        board[0][6]=new Knight(false);
+        board[1][4]=new Pawn(false);
+
+        return board;
+    }
+
+    public static Piece[][] makeRandomizedBoardThatIsAValidChess960Board(){
+        Piece[][] board=new Piece[8][8];
+        for(int i=0;i<8;i++) {
+            board[1][i]=new Pawn(false);
+            board[6][i]=new Pawn(true);
+        }
+        ArrayList<Integer> spotsLeft=new ArrayList<Integer>();
+        for(int i=0;i<8;i++)
+            spotsLeft.add(i);
+
+        //Bishop on even
+        int index=(int)(Math.random()*4)*2;
+        board[0][index]=new Bishop(false);
+        board[7][index]=new Bishop(true);
+        spotsLeft.remove((Object)new Integer(index));
+
+        //Bishop on odd
+        index=(int)(Math.random()*4)*2+1;
+        board[0][index]=new Bishop(false);
+        board[7][index]=new Bishop(true);
+        spotsLeft.remove((Object)new Integer(index));
+
+        //Queen
+        index=(int)(Math.random()*6);
+        index=spotsLeft.get(index);
+        board[0][index]=new Queen(false);
+        board[7][index]=new Queen(true);
+        spotsLeft.remove((Object)new Integer(index));
+
+        //Knights
+        index=(int)(Math.random()*5);
+        index=spotsLeft.get(index);
+        board[0][index]=new Knight(false);
+        board[7][index]=new Knight(true);
+        spotsLeft.remove((Object)new Integer(index));
+        index=(int)(Math.random()*4);
+        index=spotsLeft.get(index);
+        board[0][index]=new Knight(false);
+        board[7][index]=new Knight(true);
+        spotsLeft.remove((Object)new Integer(index));
+
+        //Everything else
+        board[0][spotsLeft.get(0)]=new Rook(false);
+        board[0][spotsLeft.get(1)]=new King(false);
+        board[0][spotsLeft.get(2)]=new Rook(false);
+        board[7][spotsLeft.get(0)]=new Rook(true);
+        board[7][spotsLeft.get(1)]=new King(true);
+        board[7][spotsLeft.get(2)]=new Rook(true);
+
+        return board;
+    }
+
 }

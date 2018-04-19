3a7ea0357d00f17f27a2c26d12f82ee1432905da
Timothy Yu <amasadpanda@gmail.com>
2018-04-19 17:14:11 -0400

    edited ui, added leavegame

--- Server/Server/src/main/java/GameLeaver.java
+++ Server/Server/src/main/java/GameLeaver.java
@@ -6,25 +6,37 @@ import java.util.Map;
 public class GameLeaver extends FireEater {
     @Override
     public CWHResponse handle(CWHRequest request) {
-        String UID = request.getExtras().get("uid");
-        String username = request.getExtras().get("username");
-        String gameID = request.getExtras().get("gameid");
+        try
+        {
+            String UID = request.getExtras().get("uid");
+            String username = request.getExtras().get("username");
+            String gameID = request.getExtras().get("gameid");
 
-        DatabaseReference ref = FireEater.getDatabase().getReference();
-        DatabaseReference game = ref.child("games").child(gameID);
+            DatabaseReference ref = FireEater.getDatabase().getReference();
+            DatabaseReference game = ref.child("games").child(gameID);
 
-        SynchronousListener s = new SynchronousListener();
-        game.addListenerForSingleValueEvent(s);
-        Game g = s.getSnapshot().getValue(Game.class);
+            SynchronousListener s = new SynchronousListener();
+            game.addListenerForSingleValueEvent(s);
+            Game g = s.getSnapshot().getValue(Game.class);
 
-        String other = (!UID.equals(g.white)?g.white:g.black);
-        ref.child("users").child(UID).child("current_games").child(gameID).removeValueAsync();
-        ref.child("users").child(UID).child("past_games").child(gameID).setValueAsync(other);
+            String other = (!UID.equals(g.white)?g.white:g.black);
+            ref.child("users").child(UID).child("current_games").child(gameID).removeValueAsync();
+            ref.child("users").child(UID).child("past_games").child(gameID).setValueAsync(other);
 
-        if(!g.turn.startsWith("winner"))
-            g.turn = "winner="+other;
+            if(!g.turn.startsWith("winner"))
+            {
+                g.turn = "winner="+other;
+                game.setValueAsync(g);
+                return new CWHResponse("You left the empty game behind", true);
+            }
 
-        game.setValueAsync(g);
-        return new CWHResponse("You have left the game, DISHONOR TO YOUR FAMILY", true);
+            game.setValueAsync(g);
+            return new CWHResponse("You have left the game, DISHONOR TO YOUR FAMILY", true);
+        }
+        catch (Exception ex)
+        {
+            return new CWHResponse("Failed to leave the game, try again!", false);
+        }
+
     }
 }
